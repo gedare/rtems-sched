899b2a95acb3ecfc5053fa8db580e5c2e87fb45e
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@34d875ca-b508-5d00-4930-5f098ad9d731>
2010-06-07 15:02:53 +0000

    scripts to help run tests and to set environment variables for path

---
+++ trunk/runtest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]
+then
+  echo "Error - parameters missing\nSyntax: $0 test_group test_name\nExample: $0 samples hello\n"
+  exit 1
+fi
+
+BSP=pc386
+TARGET=i386-rtems4.10
+EXTENSION=
+
+PWD=`pwd`
+
+$R/gcc-testing/sim-scripts/pc386 -i b-${BSP}${EXTENSION}/${TARGET}/c/${BSP}/testsuites/${1}/${2}/${2}.exe
+
