342ceb6f60894e2f1384dc687e090ae16181e381
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@34d875ca-b508-5d00-4930-5f098ad9d731>
2010-06-29 19:30:25 +0000

    updated build script

--- build-b-pc386.sh
+++ build-b-pc386.sh
@@ -10,10 +10,12 @@ then
   rm * -rf
   #../rtems/configure --target=${TARGET} --disable-posix --disable-itron --disable-networking --disable-multiprocessing --enable-rtemsbsp=${BSP}
   #../rtems/configure --target=${TARGET} --disable-networking --enable-rtemsbsp=${BSP} --disable-posix
-#  ../rtems/configure --target=${TARGET} --disable-networking --enable-rtemsbsp=${BSP} --enable-tests=samples
   #../rtems/configure --target=${TARGET} --disable-networking --enable-rtemsbsp=${BSP} --enable-tests
 
-  ## Build for QEMU
+## Build only samples
+# ../rtems/configure --target=${TARGET} --disable-networking --enable-rtemsbsp=${BSP} --enable-tests=samples
+
+## Build for QEMU
   ../rtems/configure --target=${TARGET} --disable-networking \
     --enable-rtemsbsp=${BSP} --enable-tests=samples \
     USE_COM1_AS_CONSOLE=1 BSP_PRESS_KEY_FOR_RESET=0
