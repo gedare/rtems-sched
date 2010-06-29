5f502664c80efd58699753a251aa35900a36bccf
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@34d875ca-b508-5d00-4930-5f098ad9d731>
2010-06-29 20:04:36 +0000

    remove rqdata.inl. delete readyqflush

--- rtems/cpukit/score/include/rtems/score/readyq.h
+++ rtems/cpukit/score/include/rtems/score/readyq.h
@@ -32,51 +32,78 @@ extern "C" {
 #endif
 
 #include <rtems/score/object.h>
-//#include <rtems/score/rqdata.h>
 #include <rtems/score/thread.h>
 
 #include <rtems/score/chain.h>
 #include <rtems/score/priority.h>
 
 /**
- *  The following enumerated type details all of the disciplines
- *  supported by the Ready Queue Handler.
+ * function jump table that holds pointers to the functions that 
+ * implement the ready queue structure-specific code
  */
-typedef enum {
-  READY_QUEUE_DISCIPLINE_FIFO,     /* FIFO queue discipline */
-  READY_QUEUE_DISCIPLINE_PRIORITY  /* PRIORITY queue discipline */
-}   Ready_queue_Disciplines;
+typedef struct {
+  /*
+   * removes the next ready thread from the ready queue.
+   */
+  Thread_Control * ( *dequeue )( Ready_queue_Control * );
+
+  /*
+   * enqueues the given thread on the ready queue.
+   */
+  void ( *enqueue )( Ready_queue_Control *, Thread_Control * );
+
+  /*
+   * enqueues the given thread at the head of the ready queue, respecting 
+   * the scheduling policy. e.g. for priority-based scheduling the thread
+   * will be the first thread at its priority level.
+   */
+  void ( *enqueue_first )( Ready_queue_Control *, Thread_Control * );
+
+  /*
+   * Removes and reinserts a thread on the ready queue.
+   */
+  void ( *requeue )( Ready_queue_Control *, Thread_Control * );
+
+  /*
+   * Removes a thread from the ready queue.
+   */
+  void ( *extract )( Ready_queue_Control *, Thread_Control * );
+
+  /*
+   * Returns a pointer to the first (head) thread on the ready queue.
+   */
+  Thread_Control * ( *first )( Ready_queue_Control *the_ready_queue );
+
+  /*
+   * Sets the ready pointer of the thread, using information available from 
+   * the Thread_Control and Ready_queue_Control structures.
+   */
+  void ( *set_ready )(  Ready_queue_Control *the_ready_queue, 
+      Thread_Control *the_thread );
+
+} Ready_queue_Operations;
 
 /**
  *  This is the structure used to manage sets of tasks which are ready to 
  *  use the CPU.
  */
 typedef struct {
-  /** This union contains the data structures used to manage the ready
-   *  set of tasks which varies based upon the discipline.
+  /** 
+   *  This union contains the data structures used to manage the ready
+   *  set of tasks which varies based upon the type of ready queue required
+   *  by the scheduler.
    */
   union {
-    /** This is the FIFO discipline list. */
+    /** This is for a simple FIFO list. */
     Chain_Control Fifo;
 
-    /** This is the set of lists for priority discipline scheduling. */
+    /** This is the set of lists for priority scheduling. */
     Chain_Control *Priority;
   } Queues;
 
-  /** This field indicates the ready queue's discipline. */
-  Ready_queue_Disciplines discipline;
+  /** The jump table for ready queue structure-specific functions */
+  Ready_queue_Operations rq_ops;
 }   Ready_queue_Control;
-
-
-
-/**
- *  Note: The following declarations are for currently unused functions/types.
- */
-typedef void ( *Ready_queue_Flush_callout )(
-                  Thread_Control *
-             );
-
-
 
 /** @brief  Ready queue Initialize
  *
