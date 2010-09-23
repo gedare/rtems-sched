d3ebb874b6288533dc85152bc8a9c2282d181e51
gedare@gwmail.gwu.edu <gedare@gwmail.gwu.edu@34d875ca-b508-5d00-4930-5f098ad9d731>
2010-09-23 16:38:48 +0000

    update to CVS head

---
+++ trunk/rtems/testsuites/gabtests/spspin13/macros.h
@@ -0,0 +1,19 @@
+/* macros.h
+ *
+ * This is a generated file.
+ * Use the gen-headers.lua script to create this file.
+ */
+
+/* This file was generated with the following parameters: 
+-T 1,0.027,0 -T 2,0.025,0 -T 3,0.036,0 -T 3,0.015,0 -T 4,0.010,0 -T 5,0.034,0 -T 6,0.017,0 -T 10,0.035,0 -T 10,0.017,0 -T 11,0.033,0 -T 11,0.003,0 -T 12,0.029,0 -T 12,0.023,0 -T 13,0.014,0 -T 17,0.017,0 -T 18,0.027,0 -T 18,0.006,0 -T 18,0.000,0 -T 18,0.031,0 -T 18,0.013,0 -T 19,0.033,0 -T 20,0.020,0 -T 20,0.029,0 -T 23,0.029,0 -T 23,0.023,0 -T 23,0.029,0 -T 24,0.024,0 -T 24,0.005,0 -T 25,0.019,0 -T 25,0.025,0 -T 26,0.028,0 -T 27,0.016,0 -T 29,0.034,0 -T 30,0.029,0 -T 30,0.017,0 -T 31,0.011,0 -T 31,0.001,0 -T 32,0.011,0 -T 33,0.015,0 -T 33,0.015,0 -T 34,0.024,0 -T 35,0.006,0 -T 37,0.019,0 -T 41,0.013,0 -T 42,0.001,0 -T 45,0.006,0 -T 46,0.035,0 -T 46,0.026,0 -T 49,0.021,0 -T 50,0.025,0 
+*/
+
+#ifndef __MACROS_H_
+#define __MACROS_H_
+
+#define  HP_LENGTH                  (-1)
+#define  MAX_PERIOD                 (50)
+#define  NUM_PERIODIC_TASKS         (50)
+#define  NUM_APERIODIC_TASKS        (0)
+#define  NUM_TASKS                  ( NUM_PERIODIC_TASKS + NUM_APERIODIC_TASKS )
+#endif
---
+++ trunk/rtems/testsuites/gabtests/spspin44/init.c
@@ -0,0 +1,138 @@
+/*
+ *  Copyright (c) 2010 Gedare Bloom.
+ *
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.com/license/LICENSE.
+ *
+ *  $Id$
+ */
+
+#define CONFIGURE_INIT
+#include "system.h"
+
+#include "params.h"
+#include <bsp.h>
+#include <boot/ofw.h>
+
+#define CPU_FREQ (5000000)
+
+static unsigned int get_Frequency(void)
+{
+	phandle root = ofw_find_device("/");
+	unsigned int freq;
+	if (ofw_get_property(root, "clock-frequency", &freq, sizeof(freq)) <= 0) {
+		printk("Unable to determine frequency, default: 0x%x\n",CPU_FREQ);
+		return CPU_FREQ;
+	}
+
+	return freq;
+}
+
+static void break_start_opal(void)
+{
+}
+
+uint32_t  Instructions_per_us;
+uint32_t  tasks_completed;
+uint32_t  deadlines_missed;
+uint64_t  tick_start, tick_stop;
+rtems_id  tasks_complete_sem;
+rtems_id  final_barrier;
+
+rtems_task Init(
+  rtems_task_argument argument
+)
+{
+  uint32_t          freq; 
+  uint32_t          index;
+  rtems_status_code status;
+
+  /*
+  puts( "\n\n*** TEST SPINNING ***" );
+  */
+
+  /* Calculate the frequency and Instructions_per_us */
+  freq = get_Frequency( );
+  Instructions_per_us = freq / 1000000;
+
+  tasks_completed = 0;
+  deadlines_missed = 0;
+
+  if ( Instructions_per_us == 0 ) {
+    puts( "error: CPU frequency is too low" );
+    rtems_test_exit(0);
+  }
+
+  build_task_name();
+
+  status = rtems_semaphore_create(
+      rtems_build_name( 'S', 'E', 'M', '0' ),  /* name = SEM0 */
+      0,                                       /* locked */
+      RTEMS_BINARY_SEMAPHORE | RTEMS_FIFO,     /* mutex w/desired discipline */
+      0,                                       /* IGNORED */
+      &tasks_complete_sem
+    );
+  directive_failed( status, "rtems_semaphore_create" );
+
+  status = rtems_semaphore_create(
+      rtems_build_name( 'S', 'E', 'M', '1' ),  /* name = SEM1 */
+      0,                                       /* locked */
+      RTEMS_SIMPLE_BINARY_SEMAPHORE,           /* mutex w/desired discipline */
+      0,                                       /* IGNORED */
+      &final_barrier
+    );
+  directive_failed( status, "rtems_semaphore_create" );
+
+  for ( index = 1 ; index <= NUM_TASKS ; index++ ) {
+    status = rtems_task_create(
+      Task_name[ index ],
+      Priorities[ index ],
+      RTEMS_MINIMUM_STACK_SIZE,
+      RTEMS_DEFAULT_MODES,
+      RTEMS_DEFAULT_ATTRIBUTES,
+      &Task_id[ index ]
+    );
+    directive_failed( status, "rtems_task_create loop" );
+  }
+
+  tick_start = tick_stop = 0;
+
+  for ( index = 1 ; index <= NUM_PERIODIC_TASKS ; index++ ) {
+    status = rtems_task_start( Task_id[ index ], Tasks_Periodic, index );
+    directive_failed( status, "rtems_task_start loop" );
+  }
+
+  for ( index = NUM_PERIODIC_TASKS+1 ; index <= NUM_TASKS ; index++ ) {
+    status = rtems_task_start( Task_id[ index ], Tasks_Aperiodic, index );
+    directive_failed( status, "rtems_task_start loop" );
+  }
+  /*  
+  printf("OS Ticks to run: %d ticks\n", MAX_PERIOD * 2 );
+  */
+
+  /* sleep while holding semaphore: is 10 ticks enough? */
+  rtems_task_wake_after( 10 );
+
+  /* protected by semaphore */
+  sparc64_read_tick( tick_start );
+  break_start_opal();
+
+  /* release all of the waiting tasks */
+  status = rtems_semaphore_flush( tasks_complete_sem );
+  directive_failed( status, "rtems_semaphore_flush" );
+
+  status = rtems_semaphore_release( tasks_complete_sem );
+  directive_failed( status, "rtems_semaphore_release" );
+
+  /* Should block forever */
+  status = rtems_semaphore_obtain( final_barrier, RTEMS_DEFAULT_OPTIONS, 0 );
+  directive_failed( status, "rtems_semaphore_obtain" );
+
+  puts("Init killing self\n");
+
+  status = rtems_task_delete( RTEMS_SELF );
+  directive_failed( status, "rtems_task_delete of RTEMS_SELF" );
+}
+
+/* end of file */
